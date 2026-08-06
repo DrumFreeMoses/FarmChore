d4f7826f37d887026967a10febd0b9708f828199
DrumFreeMoses <drumfreemoses@users.noreply.github.com>
2026-08-06 13:04:14 -0600

    Serve web app from relay (bypass broken GitHub Pages)

--- relay/main.go
+++ relay/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,13 +15,38 @@ func main() {
 	relay := NewRelay(os.Getenv("DATABASE_PATH"))
 	defer relay.Close()
 
-	http.HandleFunc("/", relay.HandleWebSocket)
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+	webDir := os.Getenv("WEB_DIR")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	if webDir != "" {
+		fs := http.FileServer(http.Dir(webDir))
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			if r.Header.Get("Upgrade") == "websocket" {
+				relay.HandleWebSocket(w, r)
+				return
+			}
+			if r.URL.Path == "/" || r.URL.Path == "" {
+				http.ServeFile(w, r, webDir+"/index.html")
+				return
+			}
+			if !strings.Contains(r.URL.Path, ".") {
+				http.ServeFile(w, r, webDir+"/index.html")
+				return
+			}
+			fs.ServeHTTP(w, r)
+		})
+		log.Printf("Serving web from %s", webDir)
+	} else {
+		mux.HandleFunc("/", relay.HandleWebSocket)
+	}
+
 	log.Printf("FarmChore relay listening on %s", addr)
-	if err := http.ListenAndServe(addr, nil); err != nil {
+	if err := http.ListenAndServe(addr, mux); err != nil {
 		log.Fatal(err)
 	}
 }
